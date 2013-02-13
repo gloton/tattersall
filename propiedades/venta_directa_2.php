618768bceb4a5ac7707cdb7e9b42100a1d4160e3
gloton <gloton@gmail.com>
2013-02-13 16:54:08 -0300

    25.6

--- propiedades/venta_directa_2.php
+++ propiedades/venta_directa_2.php
@@ -106,6 +106,7 @@ div.tabMain {
   border-width: 4px 2px 2px;
   padding: 0;
 }
+
 </style>
 </head>
 
@@ -165,10 +166,10 @@ div.tabMain {
 					</div>
 					<div class="banners">
 						<img id="banner_financiamiento" src="http://localhost/tattersall/images/banners/buscamos-la-propiedad-que-ud-necesita181x136.png" alt="Buscamos la propiedad que ud necesita" />
-						<img id="banner_financiamiento" src="http://localhost/tattersall/images/banners/vendemos-su-propiedad181x136.png" alt="Vendemos su propiedad" />
+						<img style="margin-top: 11px;" id="banner_financiamiento" src="http://localhost/tattersall/images/banners/vendemos-su-propiedad181x136.png" alt="Vendemos su propiedad" />
 					</div>
 					
-					<div class="banners" style="margin-top: 10px;">
+					<div class="banners" style="margin-top: 14px;">
 						<img id="banner_contactenos_rem" src="http://localhost/tattersall/images/banners/banner3_contactenos182x79.jpg" alt="" />
 					</div>		
 				</div>
